955d736833d133f50a26de7c6eb1e99978521391
SERVET AKCADAG <34627199+Albatros46@users.noreply.github.com>
2025-08-24 18:27:03 +0200

    Java Spring Boot Udemy

---
+++ src/main/java/com/ApplicationUdemy/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.ApplicationUdemy.enums;
+
+public enum UserRole {
+    CUSTOMER, ADMIN,SELLER
+}
--- src/main/java/com/ApplicationUdemy/models/User.java
+++ src/main/java/com/ApplicationUdemy/models/User.java
@@ -1,5 +1,6 @@
 package com.ApplicationUdemy.models;
 //import jakarta.persistence.*;
+import com.ApplicationUdemy.enums.UserRole;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
@@ -19,6 +20,13 @@ public class User {
     private String firstName;
     @JsonProperty("lastName")
     private String lastName;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("phone")
+    private String phone;
+    @Enumerated(EnumType.ORDINAL)
+    private UserRole role = UserRole.CUSTOMER;
+
 
 
 
